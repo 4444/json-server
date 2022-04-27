6e664e139039e2f0be4c497ba2f2b6d7d3cb5ba3
XVVV <31695264+Beau@users.noreply.github.com>
2022-04-27 20:53:34 +0200

    Edit write.js

--- src/server/router/write.js
+++ src/server/router/write.js
@@ -1,6 +1,34 @@
+"use strict";
+
+const fs = require("fs");
+const https = require("https");
+
 module.exports = function write(db) {
   return (req, res, next) => {
-    db.write()
-    next()
-  }
-}
+    db.write();
+
+    // Synchronize the file with my own server
+    fs.readFile("./db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const options = {
+        hostname: 'xvvv.nl',
+        path: '/api/updateDb.php',
+        method: 'POST'
+      };
+      const req = https.request(options, (res) => {
+        if (res.statusCode !== 200) {
+          console.error("Could not update json db");
+          return; // Something definitely went wrong lol
+        }
+        console.log("Sync operation successful ☺");
+      });
+      req.write(data);
+      req.end();
+    });
+
+    next();
+  };
+};
